cf56fd3aa17bb328e2cef7f3d8a15efddc4a108d
hailang <a1936978077@hotmail.com>
2023-04-03 21:11:29 +0800

    pass new_lr

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ extern "C" fn pln(lr: u64) {
 
 extern "C" {
     //目标相当于在my_func返回后，在main函数调用pln
-    fn my_func();
+    fn my_func(new_lr: u64);
 }
 
-// extern "C" fn my_func() {
-//     pln(63);
+// extern "C" fn my_func(new_lr: u64) {
+//     pln(new_lr);
 // }
 
 //RUSTFLAGS="--emit asm" cargo build --release 获取优化后的汇编代码
 fn main() {
-    unsafe { my_func() };
+    unsafe { my_func(pln as u64) };
 }
